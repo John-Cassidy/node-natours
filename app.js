ea30a826f7c94d5bfa82f36770db847c5f84d2cf
John Cassidy <jpcassidy@outlook.com>
2021-12-10 09:53:37 -0500

    /api/v1/tours GET request

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from server side', app: 'Natours' });
-});
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+app.get('/api/v1/tours', (req, res) => {
+  // return all tours-simple.json
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
-

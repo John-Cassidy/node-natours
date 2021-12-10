a4f636d81c84108a42268fa40e3bc9ce99e0b012
John Cassidy <jpcassidy@outlook.com>
2021-12-10 11:05:26 -0500

    api/v1/tours/:id GET

--- app.js
+++ app.js
@@ -20,6 +20,27 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  // return single tour
+  const id = req.params.id * 1;
+
+  const tour = tours.find((x) => x.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);

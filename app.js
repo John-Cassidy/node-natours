f2e9fcaddc353400f865b7612cbf1dfda40960b4
John Cassidy <jpcassidy@outlook.com>
2021-12-10 11:13:43 -0500

    api/v1/tours/:id PATCH

--- app.js
+++ app.js
@@ -60,6 +60,25 @@ app.post('/api/v1/tours', (req, res) => {
   );
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+
+  const tour = tours.find((x) => x.id === id);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);

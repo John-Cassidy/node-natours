ff03e3e57801ad1131c974cfad5c449703bcb3a1
John Cassidy <jpcassidy@outlook.com>
2021-12-11 10:24:19 -0500

    chaining-multiple-middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -16,6 +16,15 @@ exports.checkID = (req, res, next, val) => {
   }
   next();
 };
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price',
+    });
+  }
+  next();
+};
 
 // ROUTER HANDLERS
 exports.getAllTours = (req, res) => {
@@ -50,7 +59,7 @@ exports.createTour = (req, res) => {
   tours.push(newTour);
 
   fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
+    `${__dirname}/../dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     (err) => {
       res.status(201).json({

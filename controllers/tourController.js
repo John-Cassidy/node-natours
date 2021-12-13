8bcd08f4fa4e2e02176b445f98d3b3c538fd1c75
John Cassidy <jpcassidy@outlook.com>
2021-12-13 11:29:52 -0500

    filter-sort-page-limit

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,34 +3,55 @@ const Tour = require('../models/tourModel');
 // ROUTER HANDLERS
 exports.getAllTours = async (req, res) => {
   try {
-    // console.log(req.query);
-
-    // const tours = await Tour.find({
-    //   duration: 5,
-    //   difficulty: 'easy',
-    // });
-
-    // const tours = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
-
-    // const tours = await Tour.find();
-
     //#region BUILD QUERY
-    // CREATE COPY OF QUERY OBJECT
-    const queryObj = { ...req.query };
 
-    // EXCLUDE
+    // 1A) Filtering
+    const queryObj = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    // 1B) Advanced Filtering
+
+    // { difficulty: 'easy', duration: { $gte: 5 } }
+    // { difficulty: 'easy', duration: { gte: '5' } }
+    // gte, gt, lte, lt
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    // console.log(JSON.parse(queryStr));
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sort
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      this.query = query.sort('-createdAt');
+    }
+
+    // 3) Filter
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 10;
+    const skip = (page - 1) * limit;
+    this.query = query.skip(skip).limit(limit);
+
     //#endregion BUILD QUERY
 
     // EXECUTE QUERY
     const tours = await query;
+
+    // 5) Validate page exists
+    if (req.query.page) {
+      if (skip >= tours.length) throw new Error('this page does not exist');
+    }
 
     // SEND RESPONSE
     res.status(200).json({

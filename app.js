2e514970798035e19c0b87c492e9e4f6a8f7cbf7
John Cassidy <jpcassidy@outlook.com>
2021-12-29 10:30:22 -0500

    extending base template with blocks

--- app.js
+++ app.js
@@ -79,6 +79,16 @@ app.get('/', (req, res) => {
     user: 'Jones',
   });
 });
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours',
+  });
+});
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker',
+  });
+});
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);

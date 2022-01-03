09eca7bd6351cf84eef831e33049b95fafb8f3c8
John Cassidy <jpcassidy@outlook.com>
2022-01-03 09:25:15 -0500

    12 create user account page 193

--- controllers/authController.js
+++ controllers/authController.js
@@ -116,6 +116,8 @@ exports.protect = catchAsync(async (req, res, next) => {
 
   // GRANT ACCESS TO PROTECTED ROUTE
   req.user = currentUser;
+  // THERE IS A LOGGED IN USER
+  res.locals.user = currentUser;
   next();
 });
 

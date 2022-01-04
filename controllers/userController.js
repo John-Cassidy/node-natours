cc6d8658bafa781a17d5e42251e96c1d24a90456
John Cassidy <jpcassidy@outlook.com>
2022-01-04 09:50:58 -0500

    configure multer image upload to rename image and filter image files types only

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,35 @@
+const multer = require('multer');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 const factory = require('./handlerFactory');
 
+// MULTER - file upload configuration
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/img/users');
+  },
+  filename: (req, file, cb) => {
+    const ext = file.mimetype.split('/')[1];
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+  },
+});
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images.', 400), false);
+  }
+};
+// const upload = multer({ dest: 'public/img/users' }); // this will not rename image as you want
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadUserPhoto = upload.single('photo');
+
+// FILTER USER OBJECT
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
   Object.keys(obj).forEach((el) => {
@@ -11,6 +38,7 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
+// ROUTE HANDLERS
 exports.getMe = (req, res, next) => {
   req.params.id = req.user.id;
   next();
@@ -31,8 +59,8 @@ exports.getMe = (req, res, next) => {
 // });
 
 exports.updateMe = catchAsync(async (req, res, next) => {
-  console.log(req.file);
-  console.log(req.body);
+  // console.log(req.file);
+  // console.log(req.body);
   // 1) Create error if user POSTs password data
   if (req.body.password || req.body.passwordConfirm) {
     return next(
